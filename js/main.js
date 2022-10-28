67d8e95b574ecde9ddc3cd99a1605b8de36ef435
bensongardner <bgardner@wistaf.org>
2022-10-28 15:40:32 -0500

    Change left nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// Have to amke the crl btn not appear when page is scrolled up or down to the point where hte leftnav appears.
+
+// The hamburger menu is now appearing on top of the wistf logo. did remove a "centered container" div?
+
 const header = document.getElementById("site-header"),
       logo = document.getElementById("wistaf-logo");
 
@@ -8,19 +12,23 @@ let lastKnownScrollPosition = 0,
 // The problem seems to be with the class and size reassignments -- header and logo. The console log statements are working, so need to figure out why the style properties aren't showing up as changed. Could height be overridign maxheight/
     
 
+
+
 /// INSTEAD OFTHIS apparoch, make the logo disappear and make the icon appear. Add 
 // transition dissolves to both. monkey with the position as necessary. ALSO
-// be sure to be designing for mbile first (which you havne't  been :) )
+// be sure to be designing for mbile first (which you havne't  been :) 
+// MIGHT BE BETTER TO ASK ZOLTAN TO ADD THE TANSITON SINCE ITS A BEAAST.
+// Also, we don't really need the icon. Also, do we want the scroll button to appear whener the logo is? nah, prob not.)
 
 function resizeHeader(scrollSinceLast) {
     if (scrollSinceLast > 0) {
         header.classList.add("shrunk");
-        logo.classList.remove("full-size");
-        logo.classList.add("minimized"); 
+        logo.style.display = "none";
+        //logo.classList.add("minimized"); 
     } else if (window.scrollY < 5)  {
         header.classList.remove("shrunk");
-        logo.classList.remove("minimized");
-        logo.classList.add("full-size");
+        logo.style.display = "block";
+        //logo.classList.add("full-size");
     }
 }
 
